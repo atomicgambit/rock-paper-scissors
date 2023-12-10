3c934da7464e770e30b9b0eaa9d68986082fc3f9
Vito <134098951+atomicgambit@users.noreply.github.com>
2023-12-10 11:32:42 +0100

    Adds functions playRound and getComputerChoice

--- scripts.js
+++ scripts.js
@@ -3,13 +3,39 @@ function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   // Return a string based on the random number, "Rock"(if int=1), "Paper"(if int=2), or "Scissors"(if int=3)
   if (randomNumber === 1) {
-    return "Rock";
+    return "rock";
   } else if (randomNumber === 2) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
-//Test above code
-console.log(getComputerChoice());
+//Create a new function that takes two string parameters, computerSelection and playerSelection
+//Convert playerSelection to lowercase and store it in a new variable called playerMove
+//Check if player selection is a legal move by comparing it with the strings rock, paper or scissors.
+//If not legal, print "That's not a legal move!"
+//If the move is legal, compare it with computerChoice
+//If playermove equals computerselection, return "It's a draw"!
+//Write the conditions that cover all the possible combinations and return "You win/lose! X beats X"
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection.toLowerCase();
+  if (playerSelection === computerSelection) {
+    return "It's a draw!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}!`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+console.log(computerSelection);
